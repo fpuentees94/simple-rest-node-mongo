725c95cd94509d277aaeb0fff474600ac78aab4f
fredy <fpuentees94@gmail.com>
2019-07-31 19:36:10 -0500

    add mongo

--- app.js
+++ app.js
@@ -2,6 +2,19 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
+const mongoose = require('mongoose');
+
+
+mongoose.connect("mongodb+srv://products:<password>@cluster0-9d1wo.mongodb.net/test?retryWrites=true&w=majority", (err, res) => {
+
+    if (err) throw err;
+
+    console.log('Base de datos ONLINE');
+
+});
+
+
+
 app.get('/', function (req, res) {
  res.send(JSON.stringify({ Hello: 'World'}));
 });
