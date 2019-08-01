aefc7d6777355e54d1acc33f1475fca48f3a3094
fredy <fpuentees94@gmail.com>
2019-07-31 22:05:20 -0500

    change content-type

--- app.js
+++ app.js
@@ -27,15 +27,14 @@ mongoose.connect("mongodb+srv://root:1234@cluster0-9d1wo.mongodb.net/simple_ripl
 app.get('/', async function (req, res) {
 try{
  const resProducts = await products.find().exec();
+ res.setHeader("Content-Type", "application/json");
  res.send(JSON.stringify({products:resProducts}));
 }
  catch(error){
    rest.send(JSON.stringify(error));
  }
 });
-app.get('/lillo', function (req, res) {
- res.send(JSON.stringify({ Hello: 'Lillo'}));
-});
+
 app.listen(port, function () {
  console.log('Example app listening on port !');
 });
