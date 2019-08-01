5ffaac5463fbd91374132d3a15a6b21c217b1674
fredy <fpuentees94@gmail.com>
2019-07-31 21:11:43 -0500

    mongo

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
   restore_password: { type: String, trim: true, default: null }})
